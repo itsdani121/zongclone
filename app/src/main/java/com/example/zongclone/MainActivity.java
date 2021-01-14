bfa10a1b819bc3868e1ccf6bcccdf0fd7a8eef4e
adnan <adnanhaider3783@gmail.com>
2021-01-14 12:33:02 +0500

    navigation graph added

--- app/src/main/java/com/example/zongclone/MainActivity.java
+++ app/src/main/java/com/example/zongclone/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     NavController controller;
     TextView titleTV;
 
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
